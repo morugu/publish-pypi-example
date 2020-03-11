2bebdfa5be447050742dbe9f397fee5d00eb43b7
shoya shiraki <12c1055@gmail.com>
2020-03-11 21:46:27 +0900

    add pypi publish setting

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pypi-example',
+    packages=['pypi_example', 'pypi_example/extention/'],
+    version='0.0.1',
+    license='MIT',
+    install_requires=['requests'],
+    author='Shoya Shiraki',
+    author_email='shoya.shiraki@gmail.com',
+    url='https://github.com/morugu/publish-pypi-example',
+    description='Publish PyPI example',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='PyPI Python',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
